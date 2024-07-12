c8c4cf47028e3926a2f53e4d7df70ce5b5ffae19
sourav <souravhassan65@gmail.com>
2024-07-12 08:11:14 -0700

    final project added

---
+++ assets/js/app.js
@@ -0,0 +1,41 @@
+
+// Navigation section
+let nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function(){
+    let test = document.documentElement.scrollTop;
+
+    if(test > 300){
+        nav.classList.add('active');
+    }else{
+        nav.classList.remove('active');
+    }
+});
+
+
+//Icons part
+let search = document.querySelector(".search");
+let search_box = document.querySelector(".search_box");
+let cross = document.querySelector(".cross");
+let input_box = document.querySelector(".input_box");
+
+
+
+search.addEventListener('click', function(){
+
+    search_box.classList.add("search_active");
+
+    if(search_box.classList.contains("search_active")){
+        input_box.classList.add("input_active");
+    }else{
+        input_box.classList.remove("input_active");
+    }
+
+});
+
+cross.addEventListener('click',function(){
+
+    input_box.classList.remove("input_active");
+    search_box.classList.remove("search_active");
+    
+});
